d0f037f7c037c020cf3d20c46a2990e101b81cef
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-09-10 13:19:19 -0400

    add schema

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE lists (
+    id serial PRIMARY KEY,
+    name varchar(250) NOT NULL UNIQUE
+);
+
+CREATE TABLE todo (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    completed boolean NOT NULL DEFAULT false,
+    list_id integer NOT NULL references lists(id)
+)
+
